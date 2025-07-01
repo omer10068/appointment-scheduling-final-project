e1a89776aa97b7c7989b03168700af6bac13a16b
Omer Turgeman <omer10068@gmail.com>
2025-07-01 13:21:30 +0300

    yosef page

--- src/AppoitmentMainPage/Pages/MangementPage/MangementPage.tsx
+++ src/AppoitmentMainPage/Pages/MangementPage/MangementPage.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import React from "react";
 
 export default function MangementPage() {
-  return <Box sx={{width: '100%', height: '100%'}}>
+  return <Box sx={{width: '100%', height: '100vh', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
     MangementPage - Yosef
   </Box>;
 }
